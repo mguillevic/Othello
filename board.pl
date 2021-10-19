46d18f5c62e64c9d0380e5d8043d3855f0a85459
triangle-z <51131620+triangle-z@users.noreply.github.com>
2021-10-19 10:55:03 +0200

    recurssive display

--- board.pl
+++ board.pl
@@ -10,21 +10,14 @@ board([[a,_,_,_,_,_,_,y],
 opposite(x,o).
 opposite(o,x).
 
-succNum(0,1).
-succNum(1,2).
-succNum(2,3).
-succNum(3,4).
-succNum(4,5).
-succNum(5,6).
-succNum(6,7).
+printVal(V) :- var(V), write('-'), !.
+printVal(V) :- write(V).
 
-printVal(Column, Row) :- board(B), nth0(Row, B, L), nth0(Column, L, Elem), var(Elem), write('-'), !.
-printVal(Column, Row) :- board(B), nth0(Row, B, L), nth0(Column, L, Elem), write(Elem).
-
-display_row(N_row) :-  display_char(0, N_row), write('\n'), succNum(N_row, Var), display_row(Var).
-display_char(7, N_row) :-  printVal(N_char, N_row).
-display_char(N_char, N_row) :-  printVal(N_char, N_row), succNum(N_char, Var), display_char(Var, N_row).
+display_row([]).
+display_row([T|Q]) :-  display_char(T), write('\n'), display_row(Q).
+display_char([]).
+display_char([T|Q]) :-  printVal(T), display_char(Q).
 
 
-display_board :- display_row(0).
+display_board :- board(B), display_row(B).
 
