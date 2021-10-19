8565457f0c14dae17e2507455f5d9c65b5ce168b
triangle-z <51131620+triangle-z@users.noreply.github.com>
2021-10-19 11:55:57 +0200

    finish instantiation

--- board.pl
+++ board.pl
@@ -1,14 +1,13 @@
-board([[_,_,_,_,_,_,_,_],
-[_,_,_,_,_,_,_,_],
-[_,_,_,_,_,_,_,_],
-[_,_,_,x ,o ,_,_,_],
-[_,_,_,o ,x ,_,_,_],
-[_,_,_,_,_,_,_,_],
-[_,_,_,_,_,_,_,_],
-[_,_,_,_,_,_,_,_]]).
-
 opposite(x,o).
 opposite(o,x).
+
+:- dynamic board/1.
+
+init :- retractall(board), length(Board,8), assertLength(Board), assert(board(Board)),playMove(Board, 4, 4, NewBoard, x), applyIt(Board,NewBoard), playMove(Board, 3, 3, NewBoard, x), applyIt(Board,NewBoard), playMove(Board, 3, 4, NewBoard, o), applyIt(Board,NewBoard), playMove(Board, 4, 3, NewBoard, o), applyIt(Board,NewBoard), play('x').
+assertLength([]) :-write('').
+assertLength([H|Q]) :- length(H,8), assertLength(Q).
+
+play(Player) :- display_board.
 
 printVal(V) :- var(V), write('-'), !.
 printVal(V) :- write(V).
@@ -18,6 +17,8 @@ display_row([H|Q]) :-  display_char(H), write('\n'), display_row(Q).
 display_char([]).
 display_char([H|Q]) :-  printVal(H), display_char(Q).
 
+display_board :- board(Board), display_row(Board).
 display_board(B) :- display_row(B).
 
-playMove(Board, Row, Column, NewBoard, Player) :- Board=NewBoard, nth0(Row, NewBoard, L), nth0(Column, L, Player) .
+playMove(Board, Row, Column, NewBoard, Player) :- NewBoard=Board, nth0(Row, NewBoard, L), nth0(Column, L, Player) .
+applyIt(Board,NewBoard) :- retract(board(Board)), assert(board(NewBoard)).
