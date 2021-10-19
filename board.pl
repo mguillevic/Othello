85c3b988a070cf92440addd2f427fb985443d877
swaqif02 <92268002+swaqif02@users.noreply.github.com>
2021-10-19 11:11:58 +0200

    Add files via upload

--- board.pl
+++ board.pl
@@ -1,11 +1,11 @@
-board([[a,_,_,_,_,_,_,y],
+board([[_,_,_,_,_,_,_,_],
 [_,_,_,_,_,_,_,_],
 [_,_,_,_,_,_,_,_],
 [_,_,_,x ,o ,_,_,_],
 [_,_,_,o ,x ,_,_,_],
 [_,_,_,_,_,_,_,_],
-[f,_,_,_,_,_,_,_],
-[d,_,_,_,_,_,_,_]]).
+[_,_,_,_,_,_,_,_],
+[_,_,_,_,_,_,_,_]]).
 
 opposite(x,o).
 opposite(o,x).
@@ -14,10 +14,10 @@ printVal(V) :- var(V), write('-'), !.
 printVal(V) :- write(V).
 
 display_row([]).
-display_row([T|Q]) :-  display_char(T), write('\n'), display_row(Q).
+display_row([H|Q]) :-  display_char(H), write('\n'), display_row(Q).
 display_char([]).
-display_char([T|Q]) :-  printVal(T), display_char(Q).
+display_char([H|Q]) :-  printVal(H), display_char(Q).
 
+display_board(B) :- display_row(B).
 
-display_board :- board(B), display_row(B).
-
+playMove(Board, Row, Column, NewBoard, Player) :- Board=NewBoard, nth0(Row, NewBoard, L), nth0(Column, L, Player) .
