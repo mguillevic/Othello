21e38b084df7bde6044847484b90916b5d3ae452
hugito31200 <hugo.teixeiradecastro@hotmail.com>
2021-11-02 17:23:35 +0100

    Correction de la fonction d'évaluation qui comptait les cases vides par erreur, puis fonctions pour récupérer le meilleur coup possible dans une configuration donnée

--- heuristics.pl
+++ heuristics.pl
@@ -1,75 +1,73 @@
 :-consult(coefficients).
+:-consult(utils).
 
-max([X],X).
-max([T|Q],R):-max(Q,R), R>=T.
-max([T|Q],T):-max(Q,R), T>R.
 
-min([X],X).
-min([T|Q],R):-min(Q,R), R<T.
-min([T|Q],T):-min(Q,R), T=<R.
-
-compterSymboles(_,[],0).
-compterSymboles(Symbol,[Symbol|Q],Nb1):-
-     compterSymboles(Symbol,Q,Nb2),Nb1 is Nb2+1.
-
-compterSymboles(Symbol,[T|Q],Nb):-
-    T\==Symbol,compterSymboles(Symbol,Q,Nb).
-
-hasSymbol(maxPlayer,o).
-hasSymbol(minPlayer,x).
-
-otherPlayer(maxPlayer,minPlayer).
-otherPlayer(minPlayer,maxPlayer).
-
+%Fonction d'évaluation simple qui compte juste le nombre de pions que possède le joueur donné
 compterPionsJoueur(Matrix,Player,Res):-
     flatten(Matrix,Liste),hasSymbol(Player,Symbol),
     compterSymboles(Symbol,Liste,Res).
 
+%Pour savoir si une configuration est favorable ou non, on fait simplement la différence du nb de pions
 eval(Grid,Player,Res):-
     otherPlayer(Player,P2), compterPionsJoueur(Grid,Player,N1),
     compterPionsJoueur(Grid,P2,N2), Res is N1-N2.
 
-getListElement(Symbol,Liste,Index):-nth0(Index,Liste,Symbol).
-getMatrixElement(Symbol,Grid,X,Y):-
-    getRow(Grid,X,Row), getListElement(Symbol,Row,Y).
-
+%Renvoie le nb de points d'une ligne du plateau en prenant en compte des coefficients, c'est à dire que des cases sont plus intéressantes que d'autre
 evalRowWithCoeffs(_,_,_,[],0).
-evalRowWithCoeffs(X,Y,Symbol,[Symbol|Q],Res):-
-    coeffCase(X,Y,Coeff), Y1 is Y+1, evalRowWithCoeffs(X,Y1,Symbol,Q,Res2), 
-    Res is Res2+Coeff.
+evalRowWithCoeffs(X,Y,Symbol,[T|Q],Res):-
+    nonvar(T), Symbol==T,
+    coeffCase(X,Y,Coeff),Y1 is Y+1, 
+    evalRowWithCoeffs(X,Y1,Symbol,Q,Res2), Res is Res2+Coeff.
 evalRowWithCoeffs(X,Y,Symbol,[_|Q],Res):-
         Y1 is Y+1, evalRowWithCoeffs(X,Y1,Symbol,Q,Res).
 
+%Pareil que précédemment, mais appliqué à tout le plateau et pas juste à une ligne. X doit être instancié à 0.
 evalWithCoeffs(_,_,[],0).
 evalWithCoeffs(Player,X,[T|Q],Res):-
 	hasSymbol(Player,Symbol),evalRowWithCoeffs(X,0,Symbol,T,ResLine), 
     X1 is X+1, evalWithCoeffs(Player,X1,Q,Res1),
     Res is Res1+ResLine,!.
 
-getRow([Liste|_],0,Liste).
-getRow([_|NextRows],Index,Row):-
-    getRow(NextRows,I2,Row), Index is I2+1.
+liste_coordonnees(Board,List):- findall([X,Y],get_element(Board,X,Y,'Y'),List).
 
-getColumn([],_,[]).
-getColumn([T|Q],I,[R|X]):-
-    getRow(T,I,R),getColumn(Q,I,X).
+%Renvoie le triplé [X,Y,Evaluation] pour un coup aux coordonnées X,Y.
+make_triple(Board,Player,X,Y,Triple):-
+    hasSymbol(Player,Symbol),remplacer(Board,X,Y,Symbol,NewBoard),
+    evalWithCoeffs(Player,0,NewBoard,Res), Triple=[X,Y,Res].
+
+%Pour chaque coup que peut effectuer un joueur, renvoie le triplé (X,Y,Score) avec Score l'évaluation du jeu si on met le pion aux coordonnées (X,Y).
+liste_triples(Board,Player,List):-
+    example_Board(Board),
+    hasSymbol(Player,Symbol),list_possible_correct_moves(Board,Symbol,Moves),
+    findall(Triple,(get_element(Moves,X,Y,'Y'),make_triple(Board,Player,X,Y,Triple)),List).
+
+%Soit une liste de triplés de la forme (Row,Column,Eval), on récupère celui qui a la plus grande Eval.
+%On peut ainsi déterminer, parmi une liste de coups, lequel est le plus intéressant.
+maxTriple([Triple],Triple).
+maxTriple([T|Q],R):-nth0(2,T,Eval1),maxTriple(Q,R),nth0(2,R,Eval2),Eval2>=Eval1.
+maxTriple([T|Q],T):-nth0(2,T,Eval1),maxTriple(Q,R),nth0(2,R,Eval2),Eval1>Eval2.
+
+%Dans une configuration donnée, renvoie le meilleur coup que peut faire un joueur, au sens de la fonction d'évaluation
+bestMove(Player,X,Y):-
+    example_Board(Board),
+	liste_triples(Board,Player,List),maxTriple(List,Triple), 
+    nth0(0,Triple,X), nth0(1,Triple,Y).
+
+min_max(CurrentGrid,Player,Depth,Res,_,_):-
+    ((possible_to_play(CurrentGrid,Player,Possible),Possible='N');Depth=0),
+    evalWithCoeffs(Player,0,CurrentGrid,EvalJoueur), coeffJoueur(Player,Coeff),
+    Res is EvalJoueur*Coeff.
 	
-
-caseVide([T|_],0):-var(T).
-caseVide([_|Q],I):-caseVide(Q,I1),I is I1+1.
-
-caseVide(Grid,RowIndex,ColumnIndex):-
-    getRow(Grid,RowIndex,Row),caseVide(Row,ColumnIndex).
 	
-evalWithCoeffs(maxPlayer,[
-               [x,o,o,x,x,o,o,x],       -240
-               [o,x,o,o,o,x,x,x],       -150
-               [x,x,o,x,x,o,o,o],       +32
-               [o,o,x,x,o,x,o,o],       +40
-               [o,o,o,o,o,o,o,o],       +56
-               [x,x,x,x,o,x,x,x],       +2
-               [o,o,o,x,x,o,x,o],       -550
-               [x,o,x,o,x,o,x,o]],      -420
-               Res).
+# evalWithCoeffs(maxPlayer,0,[
+               # [x,o,o,x,x,o,o,x],       -240
+               # [o,x,o,o,o,x,x,x],       -150
+               # [x,x,o,x,x,o,o,o],       +32
+               # [o,o,x,x,o,x,o,o],       +40
+               # [o,o,o,o,o,o,o,o],       +56
+               # [x,x,x,x,o,x,x,x],       +2
+               # [o,o,o,x,x,o,x,o],       -550
+               # [x,o,x,o,x,o,x,o]],      -420
+               # Res).
 			   
 			  
