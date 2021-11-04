79bd0836304aeef8849b2bd39579379934419e43
MehdiAbdellaoui <63346389+MehdiAbdellaoui@users.noreply.github.com>
2021-11-04 17:02:14 +0100

    Update launchOthello.pl

--- launchOthello.pl
+++ launchOthello.pl
@@ -1,9 +1,5 @@
 l :- launchOthello.
-<<<<<<< Updated upstream
-launchOthello :- consult('C:/Users/marie/OneDrive/Documents/INSA/2021-2022/Cours/s1/ALIA/Projet_git/Othello/board.pl'), init.
-=======
 launchOthello :- consult('/Users/pc/Documents/INSA/4IF/ALIA/Code/Othello/board'), init.
->>>>>>> Stashed changes
 
 c :- consultOthello.
 consultOthello :- consult('/Users/pc/Documents/INSA/4IF/ALIA/Code/Othello/board').
