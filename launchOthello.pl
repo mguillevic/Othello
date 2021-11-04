be6ec1c9d3b82ce01112bdb6fceb6d0acbf3d25b
Mehdi <mehdi.abdellaoui.andaloussi@gmail.com>
2021-11-04 16:15:55 +0100

    Revert "Update launchOthello.pl"
    This reverts commit 3ee88f5b6757ba761bc0aaa1adeaf3c6d1658a8c.

--- launchOthello.pl
+++ launchOthello.pl
@@ -1,5 +1,5 @@
 l :- launchOthello.
-launchOthello :- consult('/Users/pc/Documents/INSA/4IF/ALIA/Code/Othello/board'), init.
+launchOthello :- consult('/Users/pc/Documents/INSA/4IF/ALIA/Code/Othello'), init.
 
 c :- consultOthello.
-consultOthello :- consult('/Users/pc/Documents/INSA/4IF/ALIA/Code/Othello/board').
+consultOthello :- consult('/Users/ahmed-os/Desktop/Informatique/DASI/Othello/board').
