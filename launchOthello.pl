5af49c31cd0c38eddf8e86445566f4e7935677c0
Mehdi <mehdi.abdellaoui.andaloussi@gmail.com>
2021-11-04 16:06:35 +0100

    gitignore of launchOthello
    Pour l'aborescence

--- launchOthello.pl
+++ launchOthello.pl
@@ -1,5 +1,5 @@
 l :- launchOthello.
-launchOthello :- consult('/Users/ahmed-os/Desktop/Informatique/DASI/Othello/board'), init.
+launchOthello :- consult('/Users/pc/Documents/INSA/4IF/ALIA/Code/Othello'), init.
 
 c :- consultOthello.
 consultOthello :- consult('/Users/ahmed-os/Desktop/Informatique/DASI/Othello/board').
