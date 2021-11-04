664e97c06fbfec2894b40dc1ce47d255496b5442
Mehdi <mehdi.abdellaoui.andaloussi@gmail.com>
2021-11-04 16:07:10 +0100

    Update launchOthello.pl

--- launchOthello.pl
+++ launchOthello.pl
@@ -1,5 +1,5 @@
 l :- launchOthello.
 launchOthello :- consult('/Users/pc/Documents/INSA/4IF/ALIA/Code/Othello'), init.
-
+TEST
 c :- consultOthello.
 consultOthello :- consult('/Users/ahmed-os/Desktop/Informatique/DASI/Othello/board').
